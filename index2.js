4866dc0434429696a80c39ff0d6a3402c50991f6
Monika755 <monikabaghumyan0@gmail.com>
2025-03-12 21:57:37 +0400

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,9 @@
+function showContent() { 
+    const url = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6078.508069460741!2d44.38890164190128!3d40.381062058525174!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x406a83a69eed55f7%3A0x1fd9410486e7669!2sSaghmosavan!5e0!3m2!1sen!2sam!4v1741799010949!5m2!1sen!2sam";
+     
+      window.open(url, "_blank");
+} 
+function showContentTwo() { 
+    const url = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3047.0260233730764!2d44.52713087513813!3d40.20848406828901!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x406abd31f37afe79%3A0xaa4e739cac13e05!2sVivaldi!5e0!3m2!1sen!2sam!4v1741801749870!5m2!1sen!2sam";
+    window.open(url, "_blank"); 
+}
